45869203babe4d45399917c1c93c26343615b695
Richard Yang <richardyang0806@gmail.com>
2026-09-02 19:36:13 -0400

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for i in range(9)]
+        cols = [set() for i in range(9)]
+        squares = [set() for i in range(9)]
+        def squareFinder(row, col):
+            return (row // 3) + (col // 3) * 3
+        
+        for row in range(9):
+            for col in range(9):
+                cur = board[row][col]
+                if cur == '.':
+                    continue
+                
+                square = squareFinder(row, col)
+                
+                if cur in rows[row] or cur in cols[col] or cur in squares[square]:
+                    return False
+                
+                rows[row].add(cur)
+                cols[col].add(cur)
+                squares[square].add(cur)
+        
+        return True
+                    
